1a6735d5e54a4fd1ec751bca305771d77ebbdf2f
davitonP <velascod@uabc.edu.mx>
2024-08-13 15:11:57 -0700

    mover el mapa y tamaño de la pahina fijp

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([31.8, -116.6], 12);
+var map = L.map('map').setView([32.45, -116.9], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -8,23 +8,33 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
 //     .openPopup();
 
-fetch('public/kml/example1.kml')
-    .then(response => response.text())
-    .then(kmlText => {
-        // Create new kml overlay
-        const parser = new DOMParser();
-        const kml = parser.parseFromString(kmlText, 'text/xml');
-        const track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
-fetch('public/kml/example2.kml')
-    .then(response => response.text())
-    .then(kmlText => {
-        // Create new kml overlay
-        const parser = new DOMParser();
-        const kml = parser.parseFromString(kmlText, 'text/xml');
-        const track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
+// fetch('public/kml/example1.kml')
+//     .then(response => response.text())
+//     .then(kmlText => {
+//         // Create new kml overlay
+//         const parser = new DOMParser();
+//         const kml = parser.parseFromString(kmlText, 'text/xml');
+//         const track = new L.KML(kml);
+//         map.addLayer(track);
+//         map.fitBounds(track.getBounds());
+//     });
+// fetch('public/kml/example2.kml')
+//     .then(response => response.text())
+//     .then(kmlText => {
+//         // Create new kml overlay
+//         const parser = new DOMParser();
+//         const kml = parser.parseFromString(kmlText, 'text/xml');
+//         const track = new L.KML(kml);
+//         map.addLayer(track);
+//         map.fitBounds(track.getBounds());
+//     });
+
+function moveMapTo(lat=31.8, long=-116, zoom=12) {
+    console.log(lat, long);
+    // map.panTo(new L.LatLng(lat, long));
+    map.setView(new L.LatLng(lat, long), zoom);
+}
+
+function sectionNotAvailable() {
+    alert('Funcionalidad no disponible');
+}
