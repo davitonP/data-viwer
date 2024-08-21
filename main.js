2bf9ffd2f5e75f8c6806226aa1a99473ef4ab178
davitonP <velascod@uabc.edu.mx>
2024-08-20 20:33:22 -0700

    Revert "archivos"
    This reverts commit 5a8719008f0035f58614c5df93ebb3f07f859339.

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 var map = L.map("map").setView([32.45, -116.9], 12);
-// var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// }).addTo(map);
-
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 
@@ -74,28 +67,27 @@ map.addControl(areaSelection);
 //     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
 //     .openPopup();
 
-fetch('public/kml/FallasBC_locales.kml')
-    .then(response => response.text())
-    .then(kmlText => {
-        // Create new kml overlay
-        const parser = new DOMParser();
-        const kml = parser.parseFromString(kmlText, 'text/xml');
-        const track = new L.KML(kml); 
-        console.log(track);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
-fetch('public/kml/fallaRegionales.kml')
-    .then(response => response.text())
-    .then(kmlText => {
-        // Create new kml overlay
-        const parser = new DOMParser();
-        const kml = parser.parseFromString(kmlText, 'text/xml');
-        const track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
-
+// fetch('public/kml/example1.kml')
+//     .then(response => response.text())
+//     .then(kmlText => {
+//         // Create new kml overlay
+//         const parser = new DOMParser();
+//         const kml = parser.parseFromString(kmlText, 'text/xml');
+//         const track = new L.KML(kml); 
+//         console.log(track);
+//         map.addLayer(track);
+//         map.fitBounds(track.getBounds());
+//     });
+// fetch('public/kml/example2.kml')
+//     .then(response => response.text())
+//     .then(kmlText => {
+//         // Create new kml overlay
+//         const parser = new DOMParser();
+//         const kml = parser.parseFromString(kmlText, 'text/xml');
+//         const track = new L.KML(kml);
+//         map.addLayer(track);
+//         map.fitBounds(track.getBounds());
+//     });
 
 function moveMapTo(lat = 31.8, long = -116, zoom = 12) {
     console.log(lat, long);
